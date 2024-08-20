39cb985088c702ad08a3c5d009357e7ce7bd37f4
manoj <bhise.manoj29@gmail.com>
2024-08-20 22:41:33 +0530

    home page added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,22 +1,58 @@
-import React from 'react'
-import '../styles/Home.css'
-import plantImg from '../images/unsplash_cLaaxa4DSnc-removebg-preview 1.png'
+import React from "react";
+import "../styles/Home.css";
+import freeHandImg1 from "../images/Vector 186.png";
+import freeHandImg2 from "../images/Vector 187.png";
+import rectangleImg from "../images/free_hand_rectangle.png";
+import plantImg from "../images/plant.png";
+import SearchIcon from "../icons/SearchIcon";
 
 const Home = () => {
   return (
-    <div className='home-container'>
-      <div className='plants'>
-        <div className='buy-plants'>
-          <p>cnjsncjsabnjkzn</p>
-          <div>chsaudhia</div>
+    <div className="home-container">
+      <div className="plants">
+        <div className="buy-plants">
+          <div>
+            <p>
+              Buy your <br /> dream plants
+            </p>
+          </div>
+          <div>
+            <p>
+              50+ <br /> <span>Plant Species</span>
+            </p>
+            <span></span>
+            <p>
+              100+ <br /> <span>Customers</span>
+            </p>
+          </div>
+          <div>
+            <input type="text" placeholder="What are you looking for?" />
+            <div>
+              <SearchIcon />
+            </div>
+          </div>
         </div>
-        <div className='plant'>
-          <img src="../" alt="" />
-          <img src='https://s3-alpha-sig.figma.com/img/d509/40be/c736256eb1f7aea1945f94fa8929b37b?Expires=1725235200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=GyW9AXObnR2ohLV2tLv93ssO6fXmFO8t61QeaT7-W-mmao0Z99cACvpdcUIGyYi2lkA9bZdWA0LqULmPSmhGmDP7RK52OyxuVRtpY3y9HT4zeOnE9ME~BVEnl016eE1Kh2ziL3RhUeFVVP-mF8wdcoDnOFwpXwbvpzmDUywSivb7MFTa1Vs06ssk75MaYWlyUwsmhNZahWuTlsT5OmwT9DDjdk26MRoY9dtO2nbqqxex4Kj6PZXMOl1eUWxRUq1iI-nhYnSV6D5Cy2mxdwaL11S5sDm8brNfOCfNq8dS5y5nnUYAGBCvlWClsj~sgTeuv9~yVNwVOStKCnb-7e~8Vw__'/>
+        <div className="plant">
+          <img
+            className="free-hand-img1"
+            src={freeHandImg1}
+            alt="free-hand-img1"
+          />
+          <img
+            className="rectangle-img"
+            src={rectangleImg}
+            alt="rectangle-img"
+          />
+          <img className="plant-img" src={plantImg} alt="plant-img" />
+          <img
+            className="free-hand-img2"
+            src={freeHandImg2}
+            alt="free-hand-img2"
+          />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
